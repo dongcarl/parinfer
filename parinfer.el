4b0671bf799588219915714468cd15c026e7b2d9
Carl Dong <dongcarl@fastmail.com>
2015-12-07 19:43:36 -0800

    Added templates for parinfer-*-mode

--- parinfer.el
+++ parinfer.el
@@ -1,10 +1,23 @@
+(defun parinfer-paren-mode
+    (&optional
+     (text (buffer-string))
+     (cursor-line (- (line-number-at-pos) 1))
+     (cursor-x (column-number-at-pos (point)))))
+
+(defun parinfer-indent-mode
+    (&optional
+     (text (buffer-string))
+     (cursor-line (- (line-number-at-pos) 1))
+     (cursor-x (column-number-at-pos (point)))))
+
 (define-minor-mode parinfer
   "Uses Parinfer to Format lispy code"
   :ligher " parinfer"
   (if parinfer
       (progn
-        (parinfer-mode-paren-mode)
-        (add-hook 'post-command-hook 'parinfer-mode-indent-mode nil t))
-    (remove-hook 'post-command-hook 'parinfer-mode-indent-mode t)))
+        (parinfer-paren-mode)
+	;; Add hook to front of hook list and make it a buffer-local hook
+        (add-hook 'post-command-hook 'parinfer-indent-mode nil t))
+    (remove-hook 'post-command-hook 'parinfer-indent-mode t)))
 
 (provide 'parinfer)
