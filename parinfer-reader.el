837886bcb9ac0e8a44dca586302365813510996a
Carl Dong <dongcarl@fastmail.com>
2015-12-11 05:20:57 -0800

    Renamed with proper prefix and finished translation from Clojure

--- parinfer-reader.el
+++ parinfer-reader.el
@@ -1,31 +1,33 @@
-(defvar matching-delims
+(require 'parinfer-util)
+
+(defvar parinfer-matching-delims
   '(("{" . "}")
     ("[" . "}")
     ("(" . ")")))
 
-(defun matching-delim (delim)
-  (other-in-association-in-alist delim matching-delims))
+(defun parinfer-matching-delim (delim)
+  (parinfer-other-in-association-in-alist delim parinfer-matching-delims))
 
-(defun other-in-association-in-alist (key-or-value alist)
+(defun parinfer-other-in-association-in-alist (key-or-value alist)
   (let ((maybe-match-key (assoc key-or-value alist))
 	(maybe-match-value (rassoc key-or-value alist)))
     (if maybe-match-key
 	(cdr maybe-match-key)
       (car maybe-match-value))))
 
-(defvar opening-delims
-  (mapcar 'car matching-delims))
+(defvar parinfer-opening-delims
+  (mapcar 'car parinfer-matching-delims))
 
-(defun opening-delim? (delim)
-  (member delim opening-delims))
+(defun parinfer-opening-delim? (delim)
+  (member delim parinfer-opening-delims))
 
-(defvar closing-delims
-  (mapcar 'cdr matching-delims))
+(defvar parinfer-closing-delims
+  (mapcar 'cdr parinfer-matching-delims))
 
-(defun closing-delims? (delim)
-  (member delim closing-delims))
+(defun parinfer-closing-delim? (delim)
+  (member delim parinfer-closing-delims))
 
-(defun whitespace? (ch)
+(defun parinfer-whitespace? (ch)
   (string-match "\\s-" ch))
 
 ;;------------------------------------------------------------------------
@@ -34,32 +36,32 @@
 ;;   State is tracked by checking last pushed character.
 ;;------------------------------------------------------------------------
 
-(defun prev-ch (stack)
+(defun parinfer-prev-ch (stack)
   (assq 'ch (car stack)))
 
-(defun escaping? (stack)
+(defun parinfer-escaping? (stack)
   "Next character will be escaped."
-  (string= "\\" (prev-ch stack)))
+  (string= "\\" (parinfer-prev-ch stack)))
 
-;; (defun in-str? (stack)
-;;   "Next character is inside a string."
-;;   (let ((ch (prev-ch (cond-> stack (escaping? stack) pop))))
-;;     (string= "\"" ch)))
+(defun parinfer-in-str? (stack)
+  "Next character is inside a string."
+  (let ((ch (parinfer-prev-ch (if (parinfer-escaping? stack)
+				  (cdr stack)
+				stack))))
+    (string= "\"" ch)))
 
-(defun in-comment? (stack)
+(defun parinfer-in-comment? (stack)
   "Next character is inside a comment."
-  (string= ";" (prev-ch stack)))
+  (string= ";" (parinfer-prev-ch stack)))
 
-(require 'parinfer-util)
-
-(defun in-code? (stack)
+(defun parinfer-in-code? (stack)
   "Next character is inside actual code."
-  (and (not (in-str? stack))
-       (not (in-comment? stack))))
+  (and (not (parinfer-in-str? stack))
+       (not (parinfer-in-comment? stack))))
 
-(defun valid-closer? (stack ch)
+(defun parinfer-valid-closer? (stack ch)
   "Determine if the given closing delimiter can be used next, assuming we are inside code."
-  (string= (prev-ch stack) (matching-delim ch)))
+  (string= (parinfer-prev-ch stack) (parinfer-matching-delim ch)))
 
 ;;------------------------------------------------------------------------
 ;; Delimiter Stack operations
@@ -69,21 +71,109 @@
 ;;   State is tracked by checking last character.
 ;;------------------------------------------------------------------------
 
-(defun push-char (state)
-  (let ((new-data (push-char* state)))
+(defun parinfer-push-char (state)
+  (let ((new-data (parinfer-push-char-delegator state)))
     (merge-with (lambda (x y) (or y x)) state new-data)))
 
-(defun push-char-tab (stack)
-  (if (not (in-str? stack))
+(defun parinfer-push-char-delegator (state)
+  (let ((stack (assoc-cdr 'stack state))
+	(backup (assoc-cdr 'backup state))
+	(ch (assoc-cdr 'ch state)))
+    (cond
+     ((parinfer-opening-delim? ch) (parinfer-push-char-open stack state))
+     ((parinfer-closing-delim? ch) (parinfer-push-char-close stack backup ch))
+     (else (parinfer-push-char-subdelegator ch stack state)))))
+
+(defun parinfer-push-char-subdelegator (ch stack state)
+  (cond
+   ((string= ch "\t") (parinfer-push-char-tab stack))
+   ((string= ch ";") (parinfer-push-char-semicolon stack state))
+   ((string= ch "\n") (parinfer-push-char-newline stack))
+   ((string= ch "\\") (parinfer-push-char-forward-slash stack state))
+   ((string= ch "\"") (parinfer-push-char-double-quotation-mark stack state))
+   (else (parinfer-push-char-default stack))))
+
+
+
+(defun parinfer-push-char-tab (stack)
+  (if (not (parinfer-in-str? stack))
       '((ch . "  "))
     nil))
 
-(defun push-char-semicolon (stack state)
+(defun parinfer-push-char-open (stack state)
   (cond
-   ((escaping? stack) '((stack . (cdr stack))))
-   ((in-code? stack) '((stack . (cons ))))
-   
-   )
-  )
+   ((parinfer-escaping? stack)
+    (list
+     (cons
+      'stack
+      (cdr stack))))
+   ((parinfer-in-code? stack)
+    (list
+     (cons 'stack (append stack (list (list
+				       (assoc 'x-pos state)
+				       (assoc 'ch state)
+				       (assoc 'indent-delta state)))))))
+   (else '())))
+
+(defun parinfer-push-char-close (stack backup ch)
+  (cond
+   ((parinfer-escaping? stack)
+    (list (cons 'stack (cdr stack))))
+   ((parinfer-in-code? stack)
+    (if (parinfer-valid-closer? stack ch)
+	(let ((opener (car stack)))
+	  (list (cons 'stack (cdr stack))
+		(cons 'backup (append backup (list opener)))))
+      (list (cons 'ch ""))))
+   (else '())))
+
+(defun parinfer-push-char-semicolon (stack state)
+  (cond
+   ((parinfer-escaping? stack)
+    (list (cons 'stack (cdr stack))))
+   ((parinfer-in-code? stack)
+    (list
+     (cons 'stack (append stack (list (list
+				       (assoc 'x-pos state)
+				       (assoc 'ch state)))))))
+   (else '())))
+
+(defun parinfer-push-char-newline (stack)
+  (let* ((stack (if (parinfer-escaping? stack)
+		    (cdr stack)
+		  stack))
+	 (stack (if (parinfer-in-comment? stack)
+		    (cdr stack)
+		  stack))))
+  (list (cons 'ch "")
+	(cons 'stack stack)))
+
+(defun parinfer-push-char-forward-slash (stack state)
+  (cond
+   ((parinfer-escaping? stack)
+    (list (cons 'stack (cdr stack))))
+   ((parinfer-in-comment? stack)
+    '())
+   (else
+    (list
+     (cons 'stack (append stack (list (list
+				       (assoc 'x-pos state)
+				       (assoc 'ch state)))))))))
+
+(defun parinfer-push-char-double-quotation-mark (stack state)
+  (cond
+   ((parinfer-escaping? stack) (list (cons 'stack (cdr stack))))
+   ((parinfer-in-str? stack) (list (cons 'stack (cdr stack))))
+   ((parinfer-in-comment? stack) '())
+   (else
+    (list
+     (cons 'stack (append stack (list (list
+				       (assoc 'x-pos state)
+				       (assoc 'ch state)))))))))
+
+(defun parinfer-push-char-default (stack)
+  (if (parinfer-escaping? stack)
+      (list (cons 'stack (cdr stack)))
+    '()))
 
 (provide 'parinfer-reader)
