6c013a5e48e91b07eba9a7c7677c970c64baf5d9
Carl Dong <dongcarl@fastmail.com>
2015-12-11 05:21:55 -0800

    Renamed with proper prefix and fixed some things

--- parinfer-utils.el
+++ parinfer-utils.el
@@ -1,46 +1,46 @@
-(defun assoc-cdr (key alist)
-  (assoc-default key alist))
+(defun parinfer-assoc-cdr (key alist)
+  (cdr (assoc key alist)))
 
-(defun merge-alist (&rest alists)
-  (reduce 'merge-alist-helper alists))
+(defun parinfer-merge-alist (&rest alists)
+  (reduce 'parinfer-merge-alist-helper alists))
 
-(defun merge-alist-helper (alist1 alist2)
+(defun parinfer-merge-alist-helper (alist1 alist2)
   (append (remove-if
 	   (lambda (pair) (assoc (car pair) alist2))
 	   alist1)
 	  alist2))
 
-(defun merge-with (func alist1 alist2)
-  (let* ((intersect-keys (intersect-keys-by-key alist1 alist2))
-	 (complement-assocs1 (complement-associations-by-keys alist1 intersect-keys))
-	 (complement-assocs2 (complement-associations-by-keys alist2 intersect-keys)))
+(defun parinfer-merge-with (func alist1 alist2)
+  (let* ((intersect-keys (parinfer-intersect-keys-by-key alist1 alist2))
+	 (complement-assocs1 (parinfer-complement-associations-by-keys alist1 intersect-keys))
+	 (complement-assocs2 (parinfer-complement-associations-by-keys alist2 intersect-keys)))
     (append
      complement-assocs1
      complement-assocs2
-     (process-intersect-keys func intersect-keys alist1 alist2))))
+     (parinfer-process-intersect-keys func intersect-keys alist1 alist2))))
 
-(defun process-intersect-keys (func intersect-keys alist1 alist2)
+(defun parinfer-process-intersect-keys (func intersect-keys alist1 alist2)
   (mapcar
    (lambda (key) (cons key
 			(funcall func
-				 (assoc-default key alist1)
-				 (assoc-default key alist2))))
+				 (parinfer-assoc-cdr key alist1)
+				 (parinfer-assoc-cdr key alist2))))
    intersect-keys))
 
-(defun complement-associations-by-keys (alist intersect-keys)
+(defun parinfer-complement-associations-by-keys (alist intersect-keys)
   (remove-if
    (lambda (association) (member (car association) intersect-keys))
    alist))
 
-(defun intersect-keys-by-key (alist1 alist2)
-  (let ((keys1 (alist-keys alist1))
-	(keys2 (alist-keys alist2)))
+(defun parinfer-intersect-keys-by-key (alist1 alist2)
+  (let ((keys1 (parinfer-alist-keys alist1))
+	(keys2 (parinfer-alist-keys alist2)))
     (intersection keys1 keys2)))
 
-(defun alist-keys (alist)
+(defun parinfer-alist-keys (alist)
   (mapcar 'car alist))
 
-(defun alist-values (alist)
+(defun parinfer-alist-values (alist)
   (mapcar 'cdr alist))
 
 (provide 'parinfer-util)
